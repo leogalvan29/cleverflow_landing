8d5ec16362d89c707e1786a103151f849319060f
leogalvan29 <contacto@paginaswebculiacan.net>
2023-08-27 23:53:10 -0600

    version1

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/**/*.html',       // Archivos HTML
+      './src/**/*.astro',        // Archivos Vue.js
+      './src/**/*.jsx',        // Archivos JSX
+      // ...agrega otras fuentes de contenido según sea necesario...
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
